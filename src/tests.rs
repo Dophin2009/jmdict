0ad56838d304de8de81fca0d031ded091788e932
Eric Zhao <21zhaoe@protonmail.com>
2020-06-11 16:37:56 -0400

    Refactor JMdict structs and parser into one module

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,11 @@
-use crate::parser;
+use crate::jmdict;
 use std::env;
 
 #[test]
-fn test_works() {
+fn jmdict_works() {
     let cwd = env::current_dir().unwrap();
     let jmdict = cwd.join("JMDict.xml");
     let path = jmdict.to_str().unwrap();
 
-    parser::full_parse(path).unwrap();
+    jmdict::full_parse(path).unwrap();
 }
